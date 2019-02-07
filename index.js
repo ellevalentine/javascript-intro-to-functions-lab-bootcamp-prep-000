7c8867a289db81e47e1503699386398e785a2c58
classy-debugger-3131 <elle.valentine@hotmail.com>
2019-02-07 02:38:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function shout(string) { return string.toUpperCase() }
 
 function whisper(string) { return string.toLowerCase() }
+
 function logShout(string) { console.log( string.toUpperCase()) }
+
 function logWhisper(string) { console.log( string.toLowerCase()) }
+
 function sayHiToGrandma (aWord) {
   var cantUnswer = "I can't hear you!";
   var yesUnswer = "YES INDEED!";
