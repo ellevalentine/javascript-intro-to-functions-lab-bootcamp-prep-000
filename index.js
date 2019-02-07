678e663bd7977532d05875a25cb134041c4bbff8
classy-debugger-3131 <elle.valentine@hotmail.com>
2019-02-07 02:47:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ function logShout(string) { console.log( string.toUpperCase()) }
 function logWhisper(string) { console.log( string.toLowerCase()) }
 
 function sayHiToGrandma (string) {
-  var cantUnswer = "I can't hear you!";
-  var yesUnswer = "YES INDEED!";
-  var lovUnswer = "I love you, too.";
+  var a = "I can't hear you!";
+  var b = "YES INDEED!";
+  var c = "I love you, too.";
   
-  if (string.toLowerCase(string) === string) { return cantUnswer; }
+  if (string.toLowerCase(string) === string) { return a; }
   
-  else if (string.toUpperCase(string) === string) { return yesUnswer; }
+  else if (string.toUpperCase(string) === string) { return b; }
   
   else if ("I love you, Grandma." === string) {
-    return lovUnswer }
+    return c }
 }
